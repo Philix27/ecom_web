f94019d99d29470daf39dd6f9a545b6a194ddeee
Philix27 <philixbob@gmail.com>
2022-12-10 09:30:25 +0100

    cleaned up

--- comps/Layout.js
+++ comps/Layout.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Head from 'next/head'
-import Footer from './globalComps/Footer/Footer'
-import Navbar from './globalComps/Navbar/Navbar'
+import React from "react";
+import Head from "next/head";
+import Footer from "./globalComps/Footer/Footer";
+import Navbar from "./globalComps/Navbar/Navbar";
 
 export default function Layout(props) {
-    return (
-        < >
-             <Head>
-                <title>Eligbue Felix</title>
-                <meta name="portfolio" content="Created by Eligbue" />
-                {/* <link rel="stylesheet" href="/styles/index.css" /> */}
-            </Head> 
-            <Navbar />
-            {props.children}    
-            <Footer/> 
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Ecommerce</title>
+        <meta
+          name="ecomerce"
+          content="Created by Adaigho Collins & Eligbue Felix"
+        />
+        {/* <link rel="stylesheet" href="/styles/index.css" /> */}
+      </Head>
+      <Navbar />
+      {props.children}
+      <Footer />
+    </>
+  );
 }
